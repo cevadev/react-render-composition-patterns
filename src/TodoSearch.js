a9a8d09c62714324d98b0d7f747de7d7dc45882d
cevadev <carlos.villanueva.altuna@gmail.com>
2023-03-08 15:03:25 -0300

    React Basics - Todo Project

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import "./TodoSearch.css";
+
 function TodoSearch() {
-  return <input type="text" placeholder="Cebolla" />;
+  return <input className="TodoSearch" type="text" placeholder="Cebolla" />;
 }
 
 export { TodoSearch };
