d00990b7dac2b3d43b2a8289fe3a8a03533ca0d7
cevadev <carlos.villanueva.altuna@gmail.com>
2023-06-30 21:40:19 -0300

    Introduction to React - Todo Project

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 
-import { TodoCounter } from "./TodoCounter.js";
-import { TodoSearch } from "./TodoSearch.js";
-import { TodoList } from "./TodoList.js";
-import { TodoItem } from "./TodoItem.js";
-import { CreateTodoButton } from "./CreateTodoButton.js";
+import { AppUI } from "./AppUI";
 
 // import "./App.css";
 
@@ -28,7 +24,7 @@ const defaultTodos = [
   },
   {
     text: "Reparar el celular",
-    completed: true,
+    completed: false,
   },
 ];
 
@@ -82,22 +78,15 @@ function App(props) {
   };
 
   return (
-    <React.Fragment>
-      <TodoCounter total={totalTodos} completed={completedTodos} />
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-      <CreateTodoButton />
-    </React.Fragment>
+    <AppUI
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
